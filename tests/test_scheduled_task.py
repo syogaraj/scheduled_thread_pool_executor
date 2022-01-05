33ab69472ba431a77fd462d75ac655c2c1cddcac
Yogaraj.S <yoga30696@gmail.com>
2022-01-05 09:08:37 +0530

    add: tests for scheduled task

--- tests/test_scheduled_task.py
+++ tests/test_scheduled_task.py
@@ -1,15 +1,20 @@
 import pytest
 
 from scheduled_thread_pool_executor.ScheduledTask import ScheduledTask
+from scheduled_thread_pool_executor.ScheduledThreadPoolExecutor import ScheduledThreadPoolExecutor
 
 
 def runnable_task():
     print("a task")
 
 
+scheduled_thread_pool_executor = ScheduledThreadPoolExecutor()
+
+
 @pytest.fixture
 def periodic_task():
-    return ScheduledTask(runnable_task, initial_delay=0, period=5)
+    return ScheduledTask(runnable_task, initial_delay=0, period=5, is_fixed_rate=True,
+                         executor_ctx=scheduled_thread_pool_executor)
 
 
 @pytest.fixture
@@ -20,10 +25,9 @@ def non_periodic_task():
 def test_schedule_task_values(periodic_task, non_periodic_task):
     assert periodic_task.initial_delay == 0
     assert periodic_task.period == 5
-    assert periodic_task.is_periodic is True
     assert periodic_task.is_initial_run is True
     assert periodic_task < non_periodic_task
-    assert non_periodic_task.is_periodic is False
+    assert periodic_task.exception_callback is None
 
     periodic_task.set_next_run()
     assert periodic_task.is_initial_run is False
@@ -35,3 +39,13 @@ def test_schedule_task_errs(periodic_task, non_periodic_task):
 
     with pytest.raises(TypeError):
         assert non_periodic_task < 10  # someother type which is not of ScheduledTask
+
+
+def test_schedule_task_run(periodic_task):
+    import time
+    st_time = time.time_ns()
+    periodic_task.run()
+    end_time = time.time_ns()
+    time_taken = (end_time - st_time) / 1000000  # in milliseconds
+    assert ((int(periodic_task.time_func() * 1000) + periodic_task.period * 1000) - time_taken)//1000 == (
+        periodic_task.task_time)//1000
